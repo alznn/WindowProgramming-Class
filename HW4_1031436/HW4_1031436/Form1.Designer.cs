08855cd6eee6def196548664f76978139f5d4bf3
alznn <zxcv19960410@gmail.com>
2018-01-18 19:55:01 +0800

    反彈球
    1.可以利用滑鼠移動板子來反彈移動的球; 
    2.狀態列呈現秒數和狀態; 
    3.具備選單可以開始新遊戲;
    4.每隔五秒，球會變快; 
    5.工具列可以改變球的顏色

---
+++ HW4_1031436/HW4_1031436/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace HW4_1031436
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.TimeLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(507, 31);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.restartToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(72, 27);
+            this.gameToolStripMenuItem.Text = "Game";
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(153, 30);
+            this.restartToolStripMenuItem.Text = "Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TimeLabel1,
+            this.StatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 577);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(507, 28);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // TimeLabel1
+            // 
+            this.TimeLabel1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.TimeLabel1.Name = "TimeLabel1";
+            this.TimeLabel1.Size = new System.Drawing.Size(52, 23);
+            this.TimeLabel1.Text = "Time";
+            // 
+            // StatusLabel2
+            // 
+            this.StatusLabel2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.StatusLabel2.Name = "StatusLabel2";
+            this.StatusLabel2.Size = new System.Drawing.Size(54, 23);
+            this.StatusLabel2.Text = "State";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 31);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(507, 30);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(47, 27);
+            this.toolStripButton1.Text = "Red";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(51, 27);
+            this.toolStripButton2.Text = "Blue";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(65, 27);
+            this.toolStripButton3.Text = "Green";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 605);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel TimeLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ HW4_1031436/HW4_1031436/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW4_1031436
+{
+    public partial class Form1 : Form
+    {
+        Rectangle rect; // 矩形區域
+        int tab_pos = 10;
+        int ballposX = 20;
+        int ballposY = 50;
+        int accX = 5;
+        int accY = 3;
+        int time = 0;
+        Color Cball = Color.Red;
+        public Form1()
+        {
+            InitializeComponent();
+            rect = new Rectangle(10, 50, 218 , 300); // 畫矩形區域       
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 100;
+            timer1.Start();
+            timer2.Interval = 1000;
+            timer2.Start();
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Brush myBrush = new SolidBrush(Color.Red); // 紅色的單色塗刷
+            Brush BrushBall = new SolidBrush(Cball); // Ball的單色塗刷
+            e.Graphics.DrawRectangle(Pens.Black, rect); // 繪出矩形
+            e.Graphics.FillRectangle(myBrush, tab_pos, 351, 30, 15); // 填一個tab
+            e.Graphics.FillEllipse(BrushBall, ballposX, ballposY, 15, 15); // 填一個ball
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            //Red
+            Cball = Color.Red;
+            this.Invalidate();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            //Blue
+            Cball = Color.Blue;
+            this.Invalidate();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            StatusLabel2.Text = "playing";
+            if ((ballposX + accX) < 10 || (ballposX + accX) > 200)
+            {
+                accX = -accX;
+            }
+            else ballposX += accX;
+            if ((ballposY + accY) > 335 || (ballposY + accY) < 50)
+            {                
+                if ((ballposY + accY) < 50) accY = -accY;
+                else if ((ballposY + accY) > 335 && ballposX > tab_pos-15 && ballposX < (tab_pos + 25))
+                {
+                    accY = -accY;
+                }
+                else
+                {
+                    ballposY = 335;
+                    accX = accY = 0;
+                    timer1.Stop();
+                    timer2.Stop();
+                    StatusLabel2.Text = "lose";
+                }
+            }
+            else
+            {
+                ballposY += accY;
+            }
+
+            TimeLabel1.Text = time.ToString();
+            
+            this.Invalidate();
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            //Green
+            Cball = Color.Green;
+            this.Invalidate();
+        }
+
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            tab_pos = e.X;
+            if (tab_pos > 200) tab_pos = 200;
+            else if (tab_pos < 10) tab_pos = 10;
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ballposX = 20;
+            ballposY = 50;
+            accX = 5;
+            accY = 3;
+            time = 0;
+            timer1.Start();
+            timer2.Start();
+            Cball = Color.Red;
+            this.Invalidate();
+        }
+        
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+           time+=1;
+            if (time % 5 == 0) {
+                if (accX > 0) accX += 1;
+                else accX -= 1;
+                if (accY > 0) accY += 1;
+                else accY -= 1;
+            }
+        }
+    }
+}
