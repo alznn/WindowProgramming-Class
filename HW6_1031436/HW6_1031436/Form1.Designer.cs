e759e5734a201bc67bf84486989e190baa02f8c3
alznn <zxcv19960410@gmail.com>
2018-01-18 19:59:52 +0800

    接水果遊戲
    在120秒內讓玩家以滑鼠移動碗來接落下的水果。同時最多三種水果。呈現倒數讀秒和接到水果數。每十秒更換背景圖片
    沒辦法好好控制同時掉水果的頻率

---
+++ HW6_1031436/HW6_1031436/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace HW6_1031436
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(978, 32);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.restartToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(72, 28);
+            this.gameToolStripMenuItem.Text = "Game";
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(210, 30);
+            this.restartToolStripMenuItem.Text = "Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 887);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Time";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(183, 887);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Received";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 923);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ HW6_1031436/HW6_1031436/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW6_1031436
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bm = new Bitmap(Properties.Resources.Bowl);
+        Point pos = new Point(); // 碗的位置
+
+        Bitmap img, imgClone; //  背景影像
+        int time1 = 0; //記錄換背景時間
+
+        Bitmap fr1 = new Bitmap(Properties.Resources.Tomato);
+        Bitmap fr2 = new Bitmap(Properties.Resources.Banana);
+        Bitmap fr3 = new Bitmap(Properties.Resources.StawBerry);
+        Point pos1 = new Point(); // 水果的位置
+        Point pos2 = new Point(); // 水果的位置
+        Point pos3 = new Point(); // 水果的位置
+        Bitmap[] bmmap = new Bitmap[3];//圖片陣列
+        int count = 0; //水果
+        int time2 = 120; //紀錄總時間
+        public Form1()
+        {
+            InitializeComponent();
+            
+            DoubleBuffered = true;
+            img = new Bitmap(Properties.Resources.FB_IMG_1435904287732);
+            bmmap = new Bitmap[3] { fr1, fr2, fr3};
+            bgImageClone();
+            fruit1Down();
+            fruit2Down();
+            fruit3Down();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time1++;
+            if (time1 % 30 < 10) {
+                img = Properties.Resources.FB_IMG_1435904287732;
+            }
+            if (time1 % 30 > 10 && time1 % 30 < 20)
+            {
+                img = Properties.Resources.FB_IMG_1435904291521;
+            }
+            if (time1 % 30 > 20 && time1 % 30 < 30)
+            {
+                img = Properties.Resources.FB_IMG_1435904296642;
+            }
+            bgImageClone();
+            this.Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(imgClone.Width*2, imgClone.Height+100);
+            //計換背景
+            timer1.Interval = 1000;
+            timer1.Start();
+
+            //倒數計時
+            timer2.Interval = 1000;
+            timer2.Start();
+
+            //掉水果
+            timer3.Interval = 1;
+            timer3.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            time2--;
+            if (time2 == 0)
+            {
+                label1.Text = "Time's UP";
+                label2.Text = "The fruit you catch is " + count;
+                pos1 = new Point(0, 0);
+                pos2 = new Point(0, 0);
+                pos3 = new Point(0, 0);
+                timer1.Stop();
+                timer2.Stop();
+                timer3.Stop();
+            }
+            else
+            {
+                label1.Text = "Time : " + time2 + "seconds";
+                label2.Text = "Recieved: " + count;
+
+            }
+            this.Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            TextureBrush bg = new TextureBrush(imgClone);
+            e.Graphics.FillRectangle(bg, 0, 0, imgClone.Width*2, imgClone.Height); // 繪出圖形
+            
+            e.Graphics.TranslateTransform(pos1.X, pos1.Y);
+            TextureBrush frb1 = new TextureBrush(fr1);
+            e.Graphics.FillRectangle(frb1, 0, 0, fr1.Width, fr1.Height); // 繪出圖形
+            
+            e.Graphics.ResetTransform();
+            e.Graphics.TranslateTransform(pos2.X, pos2.Y);
+            TextureBrush frb2 = new TextureBrush(fr2);
+            e.Graphics.FillRectangle(frb2, 0, 0, fr2.Width, fr2.Height); // 繪出圖形
+
+            e.Graphics.ResetTransform();
+            e.Graphics.TranslateTransform(pos3.X, pos3.Y);
+            TextureBrush frb3 = new TextureBrush(fr3);
+            e.Graphics.FillRectangle(frb3, 0, 0, fr3.Width, fr3.Height); // 繪出圖形
+            
+            e.Graphics.ResetTransform();
+            e.Graphics.TranslateTransform(x, 400);
+            TextureBrush tb = new TextureBrush(bm);
+            e.Graphics.FillRectangle(tb, 0,0, bm.Width, bm.Height); // 繪出圖形
+        }
+        int time3 = 0;
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            time3++;
+            pos1 = new Point(pos1.X, pos1.Y + time3 % 5);
+            pos2 = new Point(pos2.X, pos2.Y + time3 % 5);
+            pos3 = new Point(pos3.X, pos3.Y + time3 % 5);
+            check();
+            this.Invalidate();
+        }
+        int x = 0;
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Location.X > imgClone.Width * 2 - 70) {
+                x = imgClone.Width * 2 - 70;
+//                pos = new Point(imgClone.Width * 2-70, 800);
+            }
+            else if (pos.X < 0) x = 0;// pos = new Point(0, 800);
+            else x = e.Location.X; //pos = new Point(e.Location.X, 800);
+
+            this.Invalidate();
+        }
+
+        void check() {
+            if (pos1.Y >= 400) {
+                if (pos1.X - x > 35 || x - pos1.X > 35)
+                {
+                    fruit1Down();
+                }
+                else
+                {
+                    count += 1;
+                    fruit1Down();
+                }
+            }
+            if (pos2.Y >= 400)
+            {
+                if (pos2.X - x > 35 || x - pos2.X > 35)
+                {
+                    fruit2Down();
+                }
+                else
+                {
+                    count += 1;
+                    fruit2Down();
+                }
+            }
+            if (pos3.Y >= 400)
+            {
+                if (pos3.X - x > 35 || x - pos3.X > 35)
+                {
+                    fruit3Down();
+                }
+                else
+                {
+                    count += 1;
+                    fruit3Down();
+                }
+            }
+        }
+        
+
+        //-------------------------------------------------------------
+        //重新生成位置
+        void fruit1Down() {
+            int acc;
+            Random rd = new Random(Guid.NewGuid().GetHashCode());
+            int r = rd.Next(100); //產生0~8的亂數
+            int px = rd.Next(1000000);
+            acc = rd.Next(100);
+            pos1 = new Point(px%img.Width, 0);
+        }
+        void fruit2Down()
+        {
+            int acc;
+            Random rd = new Random(Guid.NewGuid().GetHashCode());
+            int r = rd.Next(100); //產生0~8的亂數
+            int px = rd.Next(400000000);
+            acc = rd.Next(100);
+            pos2 = new Point(px % img.Width, 10);
+        }
+        void fruit3Down()
+        {
+            int acc;
+            Random rd = new Random(Guid.NewGuid().GetHashCode());
+            int r = rd.Next(100); //產生0~8的亂數
+            int px = rd.Next(5000000);
+            acc = rd.Next(100);
+            pos3 = new Point(px % img.Width, 10);
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            time1 = 0;
+            time2 = 120;
+            time3 = 0;
+            label1.Text = "Time : " + time2 + "seconds";
+            label2.Text = "Recieved: " + count;
+            fruit1Down();
+            fruit2Down();
+            fruit3Down();
+
+            DoubleBuffered = true;
+            img = new Bitmap(Properties.Resources.FB_IMG_1435904287732);
+            bgImageClone();
+
+            timer1.Start();
+            timer2.Start();
+            timer3.Start();
+        }
+
+        //-----------------------------------------------------------
+        void bgImageClone()
+        {
+            imgClone = (Bitmap)img.Clone();
+            int x, y;
+            // 重設 img 的 圖素
+            for (x = 0; x < imgClone.Width; x++)
+            {
+                for (y = 0; y < imgClone.Height; y++)
+                {
+                    Color pixelColor = imgClone.GetPixel(x, y); // 得到圖素
+                    Color newColor = pixelColor;
+                    newColor = Color.FromArgb(pixelColor.A / 2, pixelColor.R / 2, pixelColor.G / 2, pixelColor.B / 2);
+                    imgClone.SetPixel(x, y, newColor); // 設定圖素
+                }
+            }
+        }
+
+    }
+
+}
