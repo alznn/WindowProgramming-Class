f08c22eb371b47e875aef58816e42d92a8362a22
alznn <zxcv19960410@gmail.com>
2018-01-18 20:03:43 +0800

    學生成績資料庫程式
    建立Students Access資料庫，StudentId (primary key) (string), StudentName (string), Gender (string), MidExam (number), FinalExam (number)。撰寫Window Form程式連結Studnets Access資料庫，計算FinalScore (MidExam和FinalExam的平均)，並依FinalScore分數顯示蘋果數(4: >=90, 3: >=80, 2: >=70, 1: >=60, 0: <60)。具備新增、修改、刪除、搜尋功能。新增資料時檢查ID是否已存在。檢查輸入的MidExam和FinalExam，必須介於0~100的整數，否則警告錯誤輸入。(沒有實作蘋果)

---
+++ HW8_1031436/HW8_1031436/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace HW8_1031436
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.studentRecordsDataSet = new HW8_1031436.StudentRecordsDataSet();
+            this.studentRecordsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.studentRecordsTableAdapter = new HW8_1031436.StudentRecordsDataSetTableAdapters.StudentRecordsTableAdapter();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.studentRecordsDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentRecordsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Students";
+            // 
+            // listBox1
+            // 
+            this.listBox1.DataSource = this.studentRecordsBindingSource;
+            this.listBox1.DisplayMember = "StudentId";
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(12, 46);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(211, 310);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // studentRecordsDataSet
+            // 
+            this.studentRecordsDataSet.DataSetName = "StudentRecordsDataSet";
+            this.studentRecordsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // studentRecordsBindingSource
+            // 
+            this.studentRecordsBindingSource.DataMember = "StudentRecords";
+            this.studentRecordsBindingSource.DataSource = this.studentRecordsDataSet;
+            // 
+            // studentRecordsTableAdapter
+            // 
+            this.studentRecordsTableAdapter.ClearBeforeFill = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(279, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(362, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(460, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Gender";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(576, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 18);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Midterm";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(670, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 18);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "FinalExam";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(793, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 18);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "FinalScore";
+            // 
+            // textBox1
+            // 
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.studentRecordsBindingSource, "StudentId", true));
+            this.textBox1.Location = new System.Drawing.Point(242, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(85, 29);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.studentRecordsBindingSource, "StudentName", true));
+            this.textBox2.Location = new System.Drawing.Point(343, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(85, 29);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.studentRecordsBindingSource, "MidExam", true));
+            this.textBox3.Location = new System.Drawing.Point(570, 49);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(85, 29);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.studentRecordsBindingSource, "FinalExam", true));
+            this.textBox4.Location = new System.Drawing.Point(673, 49);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(85, 29);
+            this.textBox4.TabIndex = 11;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.studentRecordsBindingSource, "Gender", true));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.comboBox1.Location = new System.Drawing.Point(444, 49);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(111, 26);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(810, 57);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 18);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "score";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(252, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(353, 157);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 33);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Modify";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(463, 159);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 31);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "ID",
+            "Name",
+            "Gender",
+            "Midterm",
+            "FinalExam"});
+            this.comboBox2.Location = new System.Drawing.Point(242, 273);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 26);
+            this.comboBox2.TabIndex = 17;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(411, 273);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(107, 29);
+            this.textBox5.TabIndex = 18;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(541, 273);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(78, 29);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Search";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 622);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.studentRecordsDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentRecordsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private StudentRecordsDataSet studentRecordsDataSet;
+        private System.Windows.Forms.BindingSource studentRecordsBindingSource;
+        private StudentRecordsDataSetTableAdapters.StudentRecordsTableAdapter studentRecordsTableAdapter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ HW8_1031436/HW8_1031436/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW8_1031436
+{
+    public partial class Form1 : Form
+    {
+        double final = 0.0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: 這行程式碼會將資料載入 'studentRecordsDataSet.StudentRecords' 資料表。您可以視需要進行移動或移除。
+            this.studentRecordsTableAdapter.Fill(this.studentRecordsDataSet.StudentRecords);
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((this.studentRecordsBindingSource.Find("StudentId", textBox1.Text)) != -1)
+            {
+                MessageBox.Show("This ID exists!");
+            }
+            else
+            {
+                this.studentRecordsTableAdapter.Insert(textBox1.Text, textBox2.Text,comboBox1.Text, Int32.Parse(textBox3.Text), Int32.Parse(textBox4.Text));
+                this.studentRecordsTableAdapter.Fill(this.studentRecordsDataSet.StudentRecords);
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            final = (Convert.ToDouble(textBox3.Text) + Convert.ToDouble(textBox4.Text)) / 2;
+            label8.Text = final.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.studentRecordsBindingSource.EndEdit();
+            this.studentRecordsTableAdapter.Update(this.studentRecordsDataSet);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.studentRecordsTableAdapter.Delete(textBox1.Text, textBox2.Text, comboBox1.Text, Int32.Parse(textBox3.Text), Int32.Parse(textBox4.Text));
+            this.studentRecordsTableAdapter.Fill(this.studentRecordsDataSet.StudentRecords);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int i = -1;
+            switch (comboBox2.Text)
+            {
+                case "ID":
+                    i = this.studentRecordsBindingSource.Find("StudentId", textBox5.Text);
+                    break;
+                case "Name":
+                    i = this.studentRecordsBindingSource.Find("StudentName", textBox5.Text);
+                    break;
+                case "Gender":
+                    i = this.studentRecordsBindingSource.Find("Gender", textBox5.Text);
+                    break;
+                case "Midterm":
+                    i = this.studentRecordsBindingSource.Find("MidExam", textBox5.Text);
+                    if (i < 0 || i >100)
+                        MessageBox.Show("Incorrect Score!");
+                    break;
+                case "FinalExam":
+                    i = this.studentRecordsBindingSource.Find("FinalExam", textBox5.Text);
+                    if (i < 0 || i > 100)
+                        MessageBox.Show("Incorrect Score!");
+                    break;
+            }
+            if (i != -1)
+                this.studentRecordsBindingSource.Position = i;
+            else
+                MessageBox.Show("Not found!");
+        }
+    }
+}
